a60c9809518fb5ec6a445417c7d534dc1ea6b80b
Nikita Dhole <nidhole@microsoft.com>
2020-07-17 18:25:22 +0530

    Added error handling for signup feature

--- src/react/evcard-web/src/components/SignUp.js
+++ src/react/evcard-web/src/components/SignUp.js
@@ -4,7 +4,7 @@ import axios from "axios";
 class SignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "", email: "", password: "", confirmPassword: "" };
+    this.state = { username: "", email: "", password: "", confirmPassword: "" , signupMessage: ""};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,71 +19,93 @@ class SignUp extends Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    if (this.state.password != this.state.confirmPassword) {
-      alert("password doesnt match");
+    if (this.state.password !== this.state.confirmPassword) {
+      alert("Password doesn't match");
     } else {
       const signupRequest = {
         username: this.state.username,
         email: this.state.email,
         password: this.state.password,
       };
-      const res = await axios.post(
+     await axios.post(
         "http://localhost:8080/api/v1/auth/signup",
         signupRequest
-      );
-      console.log(res);
-      //   if(res.status == 201){
-      //       if(res.data.)
-      //   }
+      ).then(response => {
+        console.log(response);
+        if(response.status === 201){
+          this.setState({username:""})
+          this.setState({email:""})
+          this.setState({password:""})
+          this.setState({confirmPassword:""}) 
+        }
+        this.setState({ signupMessage: response.data.message });
+      }).catch(err => { 
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.setState({ signupMessage: err.response.data.message });
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.setState({signupMessage:"Request failed! Please try again later!"})
+        } else {
+          // anything else
+          this.setState({signupMessage:"Request failed! Please check your internet connection!"})
+        }
+      });
+      
     }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          User Name:
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Confirm Password:
-          <input
-            type="password"
-            name="confirmPassword"
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            User Name:
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Confirm Password:
+            <input
+              type="password"
+              name="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+          <h2>{this.state.signupMessage}</h2>
+        </div>
+      </div>
     );
   }
 }
